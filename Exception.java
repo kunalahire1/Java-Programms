8c308386d863ed3e07f05ba978564bf06c4d2bcc
kunalahire1 <kunalahire9607@gmail.com>
2023-12-21 17:32:34 +0530

    Exception

---
+++ Exception.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		  
+        try {
+            
+            System.out.println("1");
+            int arr[] = new int[10];
+            int c = arr[5];
+            System.out.println("2");
+        }
+        catch(Exception e) {
+            System.out.println("3");
+        }
+        finally {
+            System.out.println("4");
+        }
+        
+        System.out.println("5");
+    }
+	}
+
